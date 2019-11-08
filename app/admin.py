c43d887ee7f5e3bfca482fe52e4163d7ef7f5b32
Student <student@donation.local>
2019-11-08 15:25:22 -0600

    Django practice for class

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app.models import Book, Transaction
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
--- app/views.py
+++ app/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from datetime import datetime
+from app.models import Book, Transaction
 
 # Create your views here.
+
+
+def home(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
+
+
+def borrow_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock == True:
+        book.in_stock = False
+        book.save()
+        messages.success(request, f"Borrowed {book.title} by {book.author}")
+        book_transaction = Transaction.objects.create(
+            date_time=datetime.now(), action="CHECKOUT", book=book
+        )
+        book_transaction.save()
+        return redirect("home")
+    elif book.in_stock == False:
+        messages.error(request, f"{book.title} by {book.author} is unavailable")
+        return redirect("home")
+
+
+def return_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock == False:
+        book.in_stock = True
+        book.save()
+        messages.success(request, f"Returned {book.title} by {book.author}")
+        book_transaction = Transaction.objects.create(
+            date_time=datetime.now(), action="CHECKIN", book=book
+        )
+        book_transaction.save()
+        return redirect("home")
+    elif book.in_stock == True:
+        messages.error(request, f"{book.title} by {book.author} is already here")
+        return redirect("home")
+
